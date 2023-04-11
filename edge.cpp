a69741d4137ce29f49cc509d9c903ba5f7115ae9
Braun_Alex <lamanasakasa@gmail.com>
2023-04-11 15:23:43 +0300

    Locating functions have been added

--- edge.cpp
+++ edge.cpp
@@ -3,9 +3,17 @@
 Edge::Edge() noexcept {}
 
 Edge::Edge(const Point& startPoint, const Point& endPoint) noexcept: start(startPoint),
-end(endPoint), rev(std::make_unique<Edge>()), prev(std::make_unique<Edge>()),
-next(std::make_unique<Edge>()) {}
+end(endPoint) {}
 
-bool Edge::operator==(std::unique_ptr<Edge> otherEdge) const noexcept {
+bool Edge::operator==(const std::shared_ptr<Edge>& otherEdge) const noexcept {
     return start == otherEdge->start && end == otherEdge->end;
+}
+
+void Edge::replace(const std::shared_ptr<Edge>& otherEdge) noexcept {
+    if (this == otherEdge.get()) {
+        return;
+    }
+    otherEdge->next->prev.reset(this);
+    this->next->prev.reset(otherEdge.get());
+    this->next.swap(otherEdge->next);
 }
--- functions.cpp
+++ functions.cpp
@@ -3,3 +3,18 @@
 double distance(const Point& a, const Point& b) {
     return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
+
+double locate(const std::shared_ptr<Edge>& edge, const Point &point) {
+    Eigen::Matrix<double, 2, 2> matrix;
+    matrix << edge->start.x - point.x, edge->start.y - point.y,
+              edge->end.x - point.x, edge->end.y - point.y;
+    return matrix.determinant();
+}
+
+bool isLeft(const std::shared_ptr<Edge>& edge, const Point &point) {
+    return locate(edge, point) < 0;
+}
+
+bool isRight(const std::shared_ptr<Edge>& edge, const Point& point) {
+    return locate(edge, point) > 0;
+}
