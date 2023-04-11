1653d5f76f1ce61c5b568a60eed69936dde3bc4f
Braun_Alex <lamanasakasa@gmail.com>
2023-04-11 18:06:25 +0300

    Functions have been added

--- functions.h
+++ functions.h
@@ -4,15 +4,17 @@
 #include "edge.h"
 #include "triangle.h"
 #include <vector>
-#include <cmath>
 #include <algorithm>
 #include <Eigen/Dense>
 
-double distance(const Point& a, const Point& b);
 double locate(const std::shared_ptr<Edge>& edge, const Point& point);
 bool isLeft(const std::shared_ptr<Edge>& edge, const Point& point);
 bool isRight(const std::shared_ptr<Edge>& edge, const Point& point);
 bool circleContains(const Point& a, const Point& b, const Point& c, const Point& point);
-std::pair<std::vector<Point>, std::vector<Edge>> triangulateDelaunay(const std::vector<Point>& points);
+std::shared_ptr<Edge> addEdge(std::vector<std::shared_ptr<Edge>>& edges, const Point& startPoint,
+             const Point& endPoint);
+std::vector<std::shared_ptr<Edge>> divideAndConquer(std::vector<std::shared_ptr<Edge>>& edges,
+                                                    const std::vector<Point>& points);
+std::vector<std::pair<Point, Point>> triangulateDelaunay(const std::vector<Point>& points);
 std::pair<std::vector<Point>, std::vector<Edge>> computeEMST(const std::pair<std::vector<Point>,
         const std::vector<Edge>>& pointsAndEdges);
--- point.h
+++ point.h
@@ -5,5 +5,6 @@ public:
     Point() noexcept;
     Point(double enteredX, double enteredY) noexcept;
     bool operator==(const Point& otherPoint) const noexcept;
+    bool operator<(const Point& otherPoint) const noexcept;
     double x, y;
 };
